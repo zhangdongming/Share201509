72a0511ba2843d039208906770544d12141d1fa7
zhangyunlei <1020311652@qq.com>
2016-01-20 23:41:54 +0800

    全日制第八组第二周作业

---
+++ 每周作业/第二周/全日制第八组/任晓鑫/任晓鑫第二周作业/js/json.js
@@ -0,0 +1,12 @@
+//为表格排序准备json格式的数据
+var jsonAry = [
+    {"name": "花千骨", "sex": 1, "age": 20, "score": 99,},
+    {"name": "白子画", "sex": 0, "age": 32, "score": 97,},
+    {"name": "夏紫薰", "sex": 1, "age": 30, "score": 89,},
+    {"name": "霓漫天", "sex": 1, "age": 21, "score": 85,},
+    {"name": "东方彧卿", "sex": 0, "age": 25, "score": 97,},
+    {"name": "杀阡陌", "sex": 0, "age": 29, "score": 96,},
+    {"name": "糖宝", "sex": 1, "age": 18, "score": 78,},
+    {"name": "孟玄朗", "sex": 0, "age": 22, "score": 80,},
+    {"name": "笙萧默", "sex": 0, "age": 31, "score": 92,}
+]
---
+++ 每周作业/第二周/全日制第八组/索欣欣/索欣欣第二周作业/表格排序2/js/json数据.js
@@ -0,0 +1,14 @@
+//为我们的表格排序准备json格式的数据
+//sex:0->男 1->女
+var jsonAry = [
+    {"name": "赵老大", "age": 24, "score": 98, "sex": 0},
+    {"name": "钱老二", "age": 26, "score": 90, "sex": 0},
+    {"name": "孙老三", "age": 18, "score": 81, "sex": 1},
+    {"name": "李小四", "age": 48, "score": 84, "sex": 1},
+    {"name": "周小五", "age": 84, "score": 48, "sex": 0},
+    {"name": "王小二", "age": 18, "score": 88, "sex": 1},
+    {"name": "寒胖胖", "age": 28, "score": 91, "sex": 1},
+    {"name": "李亚芬", "age": 38, "score": 84, "sex": 1},
+    {"name": "索小欣", "age": 22, "score": 88, "sex": 1},
+];
+
