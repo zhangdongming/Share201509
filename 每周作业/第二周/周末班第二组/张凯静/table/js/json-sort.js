eced47be170645a149f86fdc1904a70b3ebbd20f
wangyuantoy <wangyuan136@126.com>
2016-01-20 23:37:38 +0800

    周末班第二组第二周作业

---
+++ 每周作业/第二周/周末班第二组/张凯静/table/js/json-sort.js
@@ -0,0 +1,8 @@
+var jsonAry = [
+    {"name": "1号", "age": 24, "score": 98, "sex": 0},
+    {"name": "2号", "age": 22, "score": 80, "sex": 1},
+    {"name": "3号", "age": 21, "score": 88, "sex": 1},
+    {"name": "4号", "age": 58, "score": 96, "sex": 0},
+    {"name": "5号", "age": 23, "score": 90, "sex": 0},
+    {"name": "6号", "age": 30, "score": 100, "sex": 1}
+];
---
+++ 每周作业/第二周/周末班第二组/谢淑宁/sortTable/js/json-sort.js
@@ -0,0 +1,9 @@
+//sex:0-女 1-男
+var jsonAry = [
+    {"name": "Gina", "age": 34, "skill": 'Design', "sex": 0},
+    {"name": "Genius", "age": 31, "skill": 'IOS', "sex": 1},
+    {"name": "Spring", "age": 31, "skill": 'PHP', "sex": 1},
+    {"name": "Leon", "age": 35, "skill": 'PHP', "sex": 1},
+    {"name": "Maker", "age": 23, "skill": 'FE', "sex": 1},
+    {"name": "Gavin", "age": 30, "skill": 'Android', "sex": 1}
+];
