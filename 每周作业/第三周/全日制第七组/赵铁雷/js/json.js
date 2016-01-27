1b4070dbb6d9d8123e439e60581a8751bc943f0c
tielei-zhao <724236598@qq.com>
2016-01-27 23:50:01 +0800

    全日制第九期第七组第三周作业

---
+++ 每周作业/第三周/全日制第七组/赵铁雷/js/json.js
@@ -0,0 +1,29 @@
+var dataAry = [
+    {
+        logo: "img/1.jpeg"
+    },
+    {
+        logo: "img/女魔.jpeg"
+    },
+    {
+        logo: "img/男魔.jpeg"
+    },
+    {
+        logo: "img/女仙.jpeg"
+    },
+    {
+        logo: "img/男仙.jpeg"
+    },
+    {
+        logo: "img/女人.jpeg"
+    },
+    {
+        logo: "img/男人.jpeg"
+    },
+    {
+        logo: "img/女鬼.jpeg"
+    },
+    {
+        logo: "img/男鬼.jpeg"
+    }
+];
